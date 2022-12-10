e4038610dd7901e8cb166eea913436edd0510d87
David Adame Soto <david.adame.itd@gmail.com>
2022-12-10 11:58:15 -0600

    ajustes a funcionalidad de barra

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const menuHam = document.querySelector('#icon-ham')
 const closeMenu = document.querySelector('#icon-close')
 const menuOculto = document.querySelector('.menu__mobile')
+const enlaces = document.querySelectorAll('.nav__desktop--li')
 
 menuHam.addEventListener('click', () => {
     menuHam.classList.add('d-none')
@@ -15,4 +16,13 @@ closeMenu.addEventListener('click', () => {
     menuHam.classList.remove('d-none')
     menuOculto.classList.remove('menu__mobile--activo')
     document.body.style.overflow = 'scroll'
+})
+
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click',(e) => {
+        const seleccionada = e.target.children[1]
+        const arrow = e.target.children[0]
+        seleccionada.classList.toggle('sub__menu--activo')
+        arrow.classList.toggle('arrow--up')
+    })
 })
